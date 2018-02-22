c53d11c8fe2529a377acaffcf7ce2e7313fe54db
Johan Talboom <borfje@gmail.com>
2018-02-22 09:53:43 +0100

    Clipping

--- Java2D_week3/wk3_clip/src/HelloClip.java
+++ Java2D_week3/wk3_clip/src/HelloClip.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
+import java.awt.geom.Ellipse2D;
 import java.awt.geom.GeneralPath;
 import java.awt.geom.Point2D;
 import java.util.Random;
@@ -17,40 +18,17 @@ public class HelloClip extends JPanel {
 		frame.setVisible(true);
 	}
 
-	Point2D point = new Point2D.Double(100,100);
-	HelloClip()
-	{
-		addMouseMotionListener(new MouseMotionAdapter() {
-			@Override
-			public void mouseMoved(MouseEvent e) {
-				point = e.getPoint();
-				repaint();
-			}
-		});
-	}
-
 	public void paintComponent(Graphics g)
 	{
 		super.paintComponent(g);
 		Graphics2D g2d = (Graphics2D)g;
 
 
-		GeneralPath star = new GeneralPath();
-		int rad = 100;
-		star.moveTo(point.getX()+100,point.getY());
-		for(double a = 0; a < 2 * Math.PI; a += Math.PI / 6)
-		{
-			star.lineTo(
-					point.getX() + rad * Math.cos(a),
-					point.getY() + rad * Math.sin(a));
-			rad = 150 - rad;
-		}
-		star.closePath();
+		Shape shape = new Ellipse2D.Double(getWidth()/2-100, getHeight()/2-100, 200, 200);
+		g2d.draw(shape);
+		g2d.clip(shape);
 
-		g2d.draw(star);
-
-		g2d.clip(star);
-		Random r = new Random(0);
+		Random r = new Random();
 		for(int i = 0; i < 1000; i++) {
 			g2d.setPaint(Color.getHSBColor(r.nextFloat(),1,1));
 			g2d.drawLine(r.nextInt() % getWidth(), r.nextInt() % getHeight(), r.nextInt() % getWidth(), r.nextInt() % getHeight());
